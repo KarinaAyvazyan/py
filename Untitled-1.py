6b5fccfb4f92c06e66c1bfbd6a136773f68c0904
KarinaAyvazyan <ayvazyankarina1@gmail.com>
2025-08-16 20:32:52 +0400

    ch

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,26 @@
-num=int(input("Enter a number: "))  
-if num > 0:
-    print("Positive number")
-elif num == 0:
-    print("Zero")
-else:
-    print("Negative number")
+# num=int(input("Enter a number: "))  
+# if num > 0:
+#     print("Positive number")
+# elif num == 0:
+#     print("Zero")
+# else:
+#     print("Negative number")
+
+
+# user=input("Enter a name: ")
+# age=int(input("Enjkdter age: "))   
+# if age>=18 and age<=125:
+#     print(f"{user} is vote ")
+# else:
+#     print(f"{user} is not vote")
+
+
+# for i in range(1, 11):
+#    print(i)
+num1=1
+num2=100
+sum=0
+while num1 <= num2:
+    sum += num1 
+    num1 += 1
+print("Sum =", sum)   
